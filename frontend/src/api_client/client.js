53ccb081a21be27c50b589e5059dcacd62aff987
Ayman-Mohamed-ElShenawy <AymanMohamed5547@gmail.com>
2025-01-05 01:23:21 +0200

    final commit

--- frontend/src/api_client/client.js
+++ frontend/src/api_client/client.js
@@ -3,7 +3,6 @@ import router from '@/router'; // Import your router instance
 
 const apiClient = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/',
-    // baseURL: 'http://todoapp.byethost3.com/api/',
     headers: {
         "Accept": "application/json",
         'Content-Type': 'application/json',
